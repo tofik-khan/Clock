94322cb690744a8e5f1ea22874bee801a5b5cda1
tofik-khan <55766480+tofik-khan@users.noreply.github.com>
2019-11-29 09:11:12 -0800

    Updating time
    [index.html]
    - Added js/script.js in head
    - Set getTime to run on load
    
    [js/script.js]
    - Added global code into function "getTime"
    - Set string formatting by element id
    - Added a timeout to recall "getTime"

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-var d = new Date();
+function getTime(){
+    var d = new Date();
 
-var h = d.getHours();
-var m = d.getMinutes();
-var s = d.getSeconds();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    
+    var amPm = getAmPm(h);
+    
+    h = cleanTime(h);
+    m = cleanTime(m);
+    s = cleanTime(s);
+    
+    document.getElementById('time').innerHTML = "<h1>" + h + ":" + m + ":" + s + " " + amPm + "</h1>";
 
-var amPm = getAmPm(h);
-
-h = cleanTime(h);
-m = cleanTime(m);
-s = cleanTime(s);
-
-document.body.innerHTML = "<h1>" + h + ":" + m + ":" + s + " " + amPm + "</h1>";
+    var t = setTimeout(getTime, 500);
+    
+}
 
 function cleanTime (time){
     if (time < 10)
