e708e084e9728d5ccc0bf35e697c8c76280148f6
tofik-khan <55766480+tofik-khan@users.noreply.github.com>
2019-11-30 12:13:02 -0800

    Fixing time format issues
    [js/script.js]
    -Fixed time format issue:
    ---- At 12am or pm, the time was shown as 0 am/pm

--- js/script.js
+++ js/script.js
@@ -10,8 +10,14 @@ function getTime(){
     h = cleanTime(h);
     m = cleanTime(m);
     s = cleanTime(s);
+
+    h = h%12; //coverting time to 12hr format
+
+    if (h == 0){
+        h = 12; //avoiding 0 as an hour
+    }
     
-    document.getElementById('time').innerHTML = "<h1>" + h%12 + ":" + m + ":" + s + " " + amPm + "</h1>";
+    document.getElementById('time').innerHTML = "<h1>" + h + ":" + m + ":" + s + " " + amPm + "</h1>";
 
     var t = setTimeout(getTime, 500);
     
